2aba732db3f1e301ef72a23dbf1a590c2d029055
Jonas Schreiber <jonas@codelooksgood.com>
2017-05-15 18:13:22 +0200

    [u] copy google-services.json if plugin is installed before platform is added

--- src/hooks/androidAfterInstall.js
+++ src/hooks/androidAfterInstall.js
@@ -1,6 +1,11 @@
 module.exports = function(context) {
 	"use strict";
 
+	// check if android is added on platform add
+	if (context.opts.platforms !== undefined && context.opts.platforms.indexOf('android') < 0) {
+        return;
+    }
+
 	var fs = context.requireCordovaModule("fs");
 	var path = context.requireCordovaModule("path");
 	var deferral = context.requireCordovaModule("q").defer();
